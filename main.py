5fc4af3f47a72948e9cfb26bb5205eb0b8a92e7a
mervebdurna <mervebdurna@gmail.com>
2022-12-06 17:24:46 +0530

    update main.py

--- main.py
+++ main.py
@@ -27,6 +27,11 @@ if __name__=="__main__":
           data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
           data_transformation = DataTransformation(data_transformation_config = data_transformation_config,data_ingestion_artifact=data_ingestion_artifact)
           data_transformation_artifact = data_transformation.initiate_data_transformation()
+
+          #model trainer
+          model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
+          model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
+          model_trainer_artifact = model_trainer.initiate_model_trainer()
      
      
      except Exception as e:
