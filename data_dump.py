8ba2d378d9c3cdfcfb9b3cb1ea9ef3e29249dc94
mervebdurna <mervebdurna@gmail.com>
2022-12-13 16:37:51 +0530

    data dump to mongo

--- data_dump.py
+++ data_dump.py
@@ -1,11 +1,17 @@
 import pymongo
 import pandas as pd
 import json
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017")
+from dotenv import load_dotenv
+from sensor.logger import logging
+from sensor.config import mongo_client
+
+logging.info(f"Loading enviroment variable from .env file.")
+load_dotenv()
+# Provide the mongodb localhost url to connect python to mongodb from config.
+client = mongo_client
 
 DATA_FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
-DATABASE_NAME="aps"
+DATABASE_NAME="mydatabase"
 COLLECTION_NAME="sensor"
 
 
